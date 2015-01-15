70167a468313541c1ba422b6c9dd1943e70ab225
Daniel Bergey <bergey@alum.mit.edu>
2015-01-15 19:06:20 +0000

    nix: add nix build files

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages_ghcjs }:
+
+let 
+  hs = haskellPackages.override {
+        extension = self: super: rec {
+          hsPkg = pkg: version: self.callPackage "/home/bergey/code/nixHaskellVersioned/${pkg}/${version}.nix" {};
+          ghcjsCanvas = self.callPackage ../ghcjs-canvas {};
+          thisPackage = self.callPackage ./. {};
+      };
+    };
+  in
+      pkgs.lib.overrideDerivation hs.thisPackage (attrs: {
+       buildInputs = [hs.cabalInstall ] ++ attrs.buildInputs;
+ })
