0e982d6450bd090c665a13a19b869d3f4dd826c9
Daniel Bergey <bergey@alum.mit.edu>
2015-01-16 15:51:00 +0000

    test: fix tests for diagrams-1.2

--- test/Tests.hs
+++ test/Tests.hs
@@ -10,7 +10,7 @@ module Tests
         ) where
 
 import           Data.Typeable
-import           Diagrams.Coordinates ((&))
+import           Diagrams.Coordinates ((^&))
 import           Diagrams.Core.Points
 import           Diagrams.Prelude hiding (connect)
 import           Diagrams.TwoD.Text
@@ -40,7 +40,7 @@ examples =
         , Test "ellipse" $
                 unitCircle # scaleX 0.5 # rotateBy (1/6)
         , Test "arc" $
-                arc (tau/4 :: Rad) (4 * tau / 7 :: Rad)
+                arc (tau/4 @@ rad) (4 * tau / 7 @@ rad)
         , Test "Pre-defined-shapes" $
                 square 1 ||| rect 0.3 0.5 ||| eqTriangle 1 ||| roundedRect 0.7 0.4 0.1
         , Test "circle-hrule-circle" $
@@ -62,7 +62,7 @@ examples =
                         , circle 1 # fc red
                         ]
         , Test "juxtaposing1" $
-                beside (20 & 30) (circle 1 # fc orange) (circle 1.5 # fc purple)
+                beside (20 ^& 30) (circle 1 # fc orange) (circle 1.5 # fc purple)
                         # showOrigin
         , Test "juxtaposing2" $
                 let d1 = circle 1 # fc red
@@ -71,19 +71,20 @@ examples =
                                                    ===
                                                    d2  )
 
-        , Test "freeze" $
-               (square 1 ||| square 1 # freeze # scale 2
-                         ||| circle 1 # freeze # scaleX 3
-               ) # lw 0.03
+                    -- TODO port freeze example to Measure
+        -- , Test "freeze" $
+        --        (square 1 ||| square 1 # freeze # scale 2
+        --                  ||| circle 1 # freeze # scaleX 3
+        --        ) # lw 0.03
 
         , Test "line-attributes" $
-               let path = fromVertices [0 & 0, 1 & 0.3, 2 & 0, 2.2 & 0.3] # lw 0.1
-               in pad 1.1 . centerXY . vcat' with { sep = 0.1 }
+               let path = fromVertices [0 ^& 0, 1 ^& 0.3, 2 ^& 0, 2.2 ^& 0.3] # lwG 0.1
+               in pad 1.1 . centerXY . vcat' (with & sep .~ 0.1)
                   $ map (path #)
                   [ lineCap LineCapButt   . lineJoin LineJoinMiter
                   , lineCap LineCapRound  . lineJoin LineJoinRound
                   , lineCap LineCapSquare . lineJoin LineJoinBevel
-                  , dashing [0.1,0.2,0.3,0.1] 0
+                  , dashingG [0.1,0.2,0.3,0.1] 0
                   ]
 
         , Test "text-basic" $
@@ -101,14 +102,14 @@ examples =
                in  t1 =/= t2 =/= t3
 
         , Test "text-attributes" $
-               let text' s t = text t # fontSize s <> strutY (s * 1.3)
+               let text' s t = text t # fontSizeG s <> strutY (s * 1.3)
                in pad 1.1 . centerXY $
                     text' 10 "Hello" # italic
                     === text' 5 "there"  # bold # font "freeserif"
                     === text' 3 "world"  # fc green
 
         , Test "text-transforms" $
-               let eff = text "F" <> square 1 # lw 0
+               let eff = text "F" <> square 1 # lwG 0
                    ts  = [ scale (1/2), id, scale 2, scaleX 2, scaleY 2
                          , scale (-1), scaleX (-1), scaleY (-1)
                          ]
@@ -134,48 +135,48 @@ examples =
         , Test "clip" $
                 square 3
                 # fc green
-                # lw 0.05
+                # lwG 0.05
                 # clipBy (square 3.2 # rotateBy (1/10))
 
         , Test "alpha-color" $
 
                let colors  = map (blue `withOpacity`) [0.1, 0.2 .. 1.0]
-               in  hcat' with { catMethod = Distrib, sep = 1 }
+               in  hcat' (with & catMethod .~ Distrib & sep .~ 1)
                      (zipWith fcA colors (repeat (circle 1)))
 
         , Test "opacity1" $
                let s c     = square 1 # fc c
                    reds    = (s darkred ||| s red) === (s pink ||| s indianred)
-               in  hcat' with { sep = 1 } . take 4 . iterate (opacity 0.7) $ reds
+               in  hcat' (with & sep .~ 1) . take 4 . iterate (opacity 0.7) $ reds
 
         , Test "fat" $
-               unitCircle # lw 0.3 # scaleX 2 # pad 1.3
+               unitCircle # lwG 0.3 # scaleX 2 # pad 1.3
 
         , Test "connect" $ connect_example
 
         , Test "fill-line" $
-               strokeLine (fromVertices [origin, 0 & 2, 3 & 3, 4 & 1])
+               strokeLine (fromVertices [origin, 0 ^& 2, 3 ^& 3, 4 ^& 1])
                  # fc blue
 
         , Test "fill-loop" $
-               strokeLoop (fromVertices [origin, 0 & 2, 3 & 3, 4 & 1] # closeLine)
+               strokeLoop (fromVertices [origin, 0 ^& 2, 3 ^& 3, 4 ^& 1] # closeLine)
                  # fc blue
 
         , Test "line-loop" $
                fc green $
                stroke $
-               trailLike ((fromVertices [origin, 0 & 2, 3 & 3, 4 & 1] # rotateBy (1/12) # closeLine # wrapLoop) `at` origin)
+               trailLike ((fromVertices [origin, 0 ^& 2, 3 ^& 3, 4 ^& 1] # rotateBy (1/12) # closeLine # wrapLoop) `at` origin)
                <>
-               trailLike ((fromVertices [origin, 0 & 2, 3 & 3, 4 & 1] # wrapLine) `at` origin)
+               trailLike ((fromVertices [origin, 0 ^& 2, 3 ^& 3, 4 ^& 1] # wrapLine) `at` origin)
 
         ]
 
-poly_example = (poly1 ||| strutX 1 ||| poly2) # lw 0.05
+poly_example = (poly1 ||| strutX 1 ||| poly2) # lwG 0.05
   where
-          poly1 = polygon with { polyType   = PolyRegular 13 5
-                               , polyOrient = OrientV }
-          poly2 = polygon with { polyType   = PolyPolar (repeat (1/40 :: CircleFrac))
-                                       (take 40 $ cycle [2,7,4,6]) }
+          poly1 = polygon (with & polyType .~ PolyRegular 13 5
+                           & polyOrient .~ OrientV)
+          poly2 = polygon (with & polyType .~ PolyPolar  (repeat (1/40 @@ turn))
+                           (take 40 $ cycle [2,7,4,6]))
 
 data Corner = NW | NE | SW | SE
   deriving (Typeable, Eq, Ord, Show)
@@ -184,13 +185,13 @@ instance IsName Corner
 connect n1 n2
   = withName n1 $ \b1 ->
     withName n2 $ \b2 ->
-      atop ((location b1 ~~ location b2) # lc red # lw 0.05)
+      atop ((location b1 ~~ location b2) # lc red # lwG 0.05)
 
 squares =  (s # named NW ||| s # named NE)
        === (s # named SW ||| s # named SE)
-  where s = square 1 # lw 0.05
+  where s = square 1 # lwG 0.05
 
-d = hcat' with {sep = 0.5} (zipWith (|>) [0::Int ..] (replicate 5 squares))
+d = hcat' (with & sep .~ 0.5) (zipWith (|>) [0::Int ..] (replicate 5 squares))
 
 pairs = [ ((0::Int) .> NE, (2::Int) .> SW)
         , ((1::Int) .> SE, (4::Int) .> NE)
